f86fa9f3a9e690e9fe6b5e442c730ee3d2f40bdf
tommaso zanatta <74731057+tommasoz24@users.noreply.github.com>
2022-12-21 17:13:53 +0100

    update project

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const adminRoutes = require('./routes/admin');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/', adminRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'roadmap.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 app.listen(5000);
--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,17 @@ const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'roadmap.html'));
+  res.sendFile(path.join(rootDir, 'views', 'index.html'));
+});
+
+// add match.html path
+router.get('/match', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'match.html'));
+  
+});
+// also if is match.html
+router.get('/match.html', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'match.html'));
 });
 
 
