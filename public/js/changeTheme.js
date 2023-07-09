87a9e7754d07cb10de80c5806446f27c734a7e96
tommasoz24 <ztommaso24004@gmail.com>
2023-07-09 21:10:56 +0200

    Update changeTheme.js

--- public/js/changeTheme.js
+++ public/js/changeTheme.js
@@ -6,45 +6,46 @@ function setTheme(themeName) {
 function toggleTheme() {
     if (localStorage.getItem('theme') === 'theme-dark'){
         setTheme('theme-light');
-        document.getElementById('icona-tema').src = "icon/dark_mode.svg";
+        document.getElementById('icona-tema').src = "/icon/dark_mode.svg";
     } 
     else {
         setTheme('theme-dark');        
-        document.getElementById('icona-tema').src = "icon/dark_mode_filled.svg";
+        document.getElementById('icona-tema').src = "/icon/dark_mode_filled.svg";
     }
 }
 
 function start() {
+    console.log(document.title);
     if (localStorage.getItem('theme') === 'theme-dark'){
         if(document.title === 'Login | WolfBet' || document.title === 'Cambio Password | WolfBet' || 
         document.title === 'Registrati | WolfBet' || document.title === 'Scelta match | WolfBet' || 
-        document.title === 'Blog | WolfBet' || document.title === 'Landing | WolfBet') {
+        document.title === 'Blog | WolfBet' || document.title === 'WolfBet') {
             document.getElementById('logo-header').src = "img/logo_scrittaChiaro.svg";
         } 
         else {
-            document.getElementById('logo-header').src = "img/logo_temaScuro.svg";
+            document.getElementById('logo-header').src = "/img/logo_temaScuro.svg";
         }
 
         if(document.title === 'Login | WolfBet' || document.title === 'Utilizzo | WolfBet' || 
         document.title === 'Registrati | WolfBet' || document.title === 'Roadmap | WolfBet' ||
         document.title === 'Scelta Piano | WolfBet') {
-            document.getElementById('logo-footer').src = "img/logo_temaScuro.svg";
+            document.getElementById('logo-footer').src = "/img/logo_temaScuro.svg";
         } 
         
     } else {
         if(document.title === 'Login | WolfBet' || document.title === 'Cambio Password | WolfBet' || 
         document.title === 'Registrati | WolfBet' || document.title === 'Scelta match | WolfBet'|| 
-        document.title === 'Blog | WolfBet' || document.title === 'Landing | WolfBet') {
-            document.getElementById('logo-header').src = "img/logo_scrittaScuro.svg";
+        document.title === 'Blog | WolfBet' || document.title === 'WolfBet') {
+            document.getElementById('logo-header').src = "/img/logo_scrittaScuro.svg";
         } 
         else {
-            document.getElementById('logo-header').src = "img/logo_temaChiaro.svg";
+            document.getElementById('logo-header').src = "/img/logo_temaChiaro.svg";
         }
 
         if(document.title === 'Login | WolfBet' || document.title === 'Utilizzo | WolfBet' || 
         document.title === 'Registrati | WolfBet' || document.title === 'Roadmap | WolfBet' ||
         document.title === 'Scelta Piano | WolfBet') {
-            document.getElementById('logo-footer').src = "img/logo_temaChiaro.svg";
+            document.getElementById('logo-footer').src = "/img/logo_temaChiaro.svg";
         } 
     }
   }
